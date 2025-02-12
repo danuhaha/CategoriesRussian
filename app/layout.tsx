33b2b182ff970b4f49e43bd36953d0ff68c6b1b5
Daniel <dgorinmail@mail.ru>
2025-02-12 16:38:22 +0300

    Change font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({
+  subsets: ["latin", "cyrillic"], // Include Cyrillic subset for Russian
+  weight: ["400", "500", "700"], // Specify font weights
+});
+
 
 export const metadata: Metadata = {
   title: "Категории",
@@ -15,8 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ru">
+      <body className={roboto.className}>{children}</body>
     </html>
   );
 }
